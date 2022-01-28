a05dab2bab69a6af8d00a1d0cac960091b313bf1
Carl Q <43070698+cquinard@users.noreply.github.com>
2022-01-28 10:06:57 -0700

    Add aliases file
    Handy aliases when working with unix

---
+++ handy_aliases.sh
@@ -0,0 +1,6 @@
+export PS1="\[\e[31m\]\$?\[\e[m\] \[\e[33m\]\$(date +%Y-%m-%d\ %H:%M:%S)\[\e[m\] \[\e[36m\]\u@\h:\$(pwd)\[\e[m\] $ "
+export HISTTIMEFORMAT="%h/%d - %H:%M "
+export HISTSIZE=10000
+export HISTFILESIZE=1000000
+alias ll="ls -lah"
+alias lls="ls -larth"
