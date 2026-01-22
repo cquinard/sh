c12d88fd2115e28b8e5a46062f889102f1ed9633
Carl Q <43070698+cquinard@users.noreply.github.com>
2026-01-22 12:33:53 -0700

    Add user and pass auth
    Add user and pass authentication to ldap search

--- query_activedirectory.sh
+++ query_activedirectory.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # bash script to issue a lookup in active directory by username / sAMAccountName for macOS
 if [ "$1" ]; then
-	ldapsearch -h domaincontroller.com -p 389 -o ldif-wrap=no -LLL -Q -b "DC=domain,DC=AD" "sAMAccountName=$1" |sort
+	# ldapsearch -h domaincontroller.com -p 389 -o ldif-wrap=no -LLL -Q -b "DC=domain,DC=AD" "sAMAccountName=$1" |sort
+	ldapsearch -a never -h domaincontroller.com -p 389 -D "your_full_DN_here" -w "password_here" -LLL  -o ldif-wrap=no -b "DC=domain,DC=AD" "sAMAccountName=$1" | sort
 else
 	echo "Specify sAMAccountName of user to query."
 fi
