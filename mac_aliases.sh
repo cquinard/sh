cb65b5c27ff19c89ae299815b1595ecae089a18c
Carl Q <43070698+cquinard@users.noreply.github.com>
2026-03-20 15:26:52 -0600

    Add mac_aliases

---
+++ mac_aliases.sh
@@ -0,0 +1,6 @@
+alias lls='ls -larthG --color=yes '
+alias ll='ls -lahG --color=yes ' 
+alias myip='curl --silent -kL https://api.ipify.org/ ; echo'
+alias d='killall Dock'
+alias b="echo -ne '\a'"
+export PS1="%F{red}%?%f %F{yellow}%D %*%f %F{cyan}%n@%m:%/%f %% "
